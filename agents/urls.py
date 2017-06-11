b74bd75f32eb4c739c7694d801c37064bd881654
Edgar Sousa <edgarmsousa@gmail.com>
2017-06-11 21:15:04 +0100

    Add default base location - urlpattern

--- agents/urls.py
+++ agents/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'register/(?P<backend>[^/]+)/$', views.API.as_view({'get': 'register'})),
-    url(r'register_device/$', views.API.as_view({'get': 'register_device'})),
-    url(r'location/$', views.API.as_view({'get': 'location', 'post': 'location'})),
+    url(r'register_device$', views.API.as_view({'get': 'register_device'})),
+    url(r'location$', views.API.as_view({'get': 'location', 'post': 'location'})),
+    url(r'base_location$', views.API.as_view({'get': 'base_location'})),
 ]
